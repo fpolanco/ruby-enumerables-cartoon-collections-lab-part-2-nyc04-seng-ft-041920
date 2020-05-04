5d572af128cede605a8cb6a1bcb6ea1399ae08cd
Franchell Polanco <polanco.franchell@gmail.com>
2020-05-04 14:40:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,9 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find_index { |each| each == valid_calls}
+  planeteer_calls.find_index { |e| e == valid_calls}
 
-   p valid_calls
+   
  end
 
 
